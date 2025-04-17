9081da5bd4e018564fc61364d1481b7f2f9706ad
LENOVO <LENOVO@DESKTOP-LMBJ13J>
2025-04-17 11:10:39 +0530

    new commite

---
+++ GithubProject/src/test/java/vivek/First.java
@@ -0,0 +1,13 @@
+package vivek;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class First {
+	@Test
+	public void pull()
+	{
+		Reporter.log("Pull request is Sucessfully");
+	}
+
+}
