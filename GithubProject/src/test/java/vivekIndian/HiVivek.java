466849708005e072f0690a693a3866cd55a088dc
LENOVO <LENOVO@DESKTOP-LMBJ13J>
2025-04-17 11:35:28 +0530

    vivek commited

---
+++ GithubProject/src/test/java/vivekIndian/HiVivek.java
@@ -0,0 +1,12 @@
+package vivekIndian;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class HiVivek {
+	@Test
+	public void shashiRanjan() {
+		Reporter.log("shashi");
+	}
+
+}
